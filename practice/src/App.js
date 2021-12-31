211ed499e264dae6b1be88b193d4fb1f58163d01
Muhammad Anis <anisiqbal484@gmail.com>
2021-12-31 17:42:16 +0500

    hhh

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import SideMenu from '../src/Components/SideMenu'
+import { makeStyles } from '@material-ui/core';
 import Header from './Components/Header';
 
+const useStyles = makeStyles({
+  appMain:{
+    paddingLeft:'320px',
+    width: "100%",
+  }
+})
+
 function App() {
+
+  const classes = useStyles();
   return (
     <>
       <SideMenu/>
+      <div className={classes.appMain}>
       <Header/>
+      </div>
+     
 
     </>
   );
