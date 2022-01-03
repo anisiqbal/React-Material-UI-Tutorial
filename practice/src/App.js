340a1e139b9a4651173e7497671167f24df23053
Muhammad Anis <anisiqbal484@gmail.com>
2022-01-04 00:19:31 +0500

    worked on pageHeader and css page header , pageIcon , pagetitle, passing props in pageHeader to App.js

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,8 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import SideMenu from '../src/Components/SideMenu'
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
 import Header from './Components/Header';
+import PageHeader from './Components/PageHeader';
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+
+
+const theme = createMuiTheme({
+  palette:{
+    primary:{
+      main:"#333996",
+      light:"#3c44b126",
+    },
+    secondary:{
+      main:"#f83245",
+      light:"#f8324526",
+    },
+    background:{
+      default:"#f4f5fd"
+    }
+  }
+})
 
 const useStyles = makeStyles({
   appMain:{
@@ -15,15 +34,21 @@ function App() {
 
   const classes = useStyles();
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu/>
       <div className={classes.appMain}>
       <Header/>
+      <PageHeader
+        title="Page Header"
+        subtitle="page description"
+        icon={<PeopleOutlineIcon/>}
+
+      />
       </div>
       <CssBaseline/>
      
 
-    </>
+    </ThemeProvider>
   );
 }
 
