0d4cb21fab6aec2aeb756d5020e8f5aed39633c5
Muhammad Anis <anisiqbal484@gmail.com>
2022-01-05 16:36:08 +0500

    worked on Reusable component in EmployeeForm and also use maping

---
+++ practice/src/Components/Controls/Controls.js
@@ -0,0 +1,13 @@
+
+// Add input Component to the object//
+
+
+import Input from "./Input"
+import RadioGroup from "./RadioGroup"
+
+ const Controls={
+    Input,
+    RadioGroup
+}
+
+export default Controls;
--- practice/src/Pages/Employees/EmployeeForm.js
+++ practice/src/Pages/Employees/EmployeeForm.js
@@ -1,23 +1,29 @@
-import { FormControl, Grid } from "@material-ui/core";
+import { FormLabel, RadioGroup, FormControl, FormControlLabel, Radio, Grid, } from "@material-ui/core";
+import Controls from "../../Components/Controls/Controls";
+
 import React, { useState, useEffect } from "react";
 import { TextField } from "@material-ui/core";
-import {UseForm, Form} from "../../Components/UseForm";
+import { UseForm, Form } from "../../Components/UseForm";
 
 
-
+const genderItems = [
+    { id: 'male', title: 'Male' },
+    { id: 'female', title: 'Female' },
+    { id: 'other', title: 'Other' },
+]
 
 
 
 const initialFValues = {
-    id:0,
-    fullName:'',
-    email:'',
-    mobile:'',
-    city:'',
-    gender:'male',
-    departmendId:'',
-    hireDate:new Date(),
-    isPermanent:false,
+    id: 0,
+    fullName: '',
+    email: '',
+    mobile: '',
+    city: '',
+    gender: 'male',
+    departmendId: '',
+    hireDate: new Date(),
+    isPermanent: false,
 }
 
 function EmployeeForm() {
@@ -25,37 +31,47 @@ function EmployeeForm() {
     const {
         values,
         setValues,
-        handleInputChange, 
-    }  = UseForm( initialFValues);
+        handleInputChange,
+    } = UseForm(initialFValues);
 
     return (
-        
-            <Form>
-                <Grid container>
-                    <Grid item xs={6}>
-                        <TextField
-                            variant="outlined"
-                            label="full Name"
-                            name="fullName"
-                            value={values.fullName}
-                            onChange={handleInputChange}
-                        />
-                        <TextField
-                            variant="outlined"
-                            label="Email"
-                            name="email"
-                            value={values.email}
-                            onChange={handleInputChange}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <FormControl>
-                            
-                        </FormControl>
-                    </Grid>
+
+        <Form>
+            <Grid container>
+                <Grid item xs={6}>
+                    <Controls.Input
+                        name="fullName"
+                        label="Full Name"
+                        value={values.fullName}
+                        onChange={handleInputChange}
+                    />
+
+                    <Controls.Input
+
+                        label="Email"
+                        name="email"
+                        value={values.email}
+                        onChange={handleInputChange}
+                    />
+
                 </Grid>
-                </Form>
-       
+                <Grid item xs={6}>
+
+                    <Controls.RadioGroup
+
+                        name="gender"
+                        label="Gender"
+                        value={values.gender}
+                        onChange={handleInputChange}
+                        items={genderItems}
+                    />
+
+
+                   
+                </Grid>
+            </Grid>
+        </Form>
+
     )
 }
 
