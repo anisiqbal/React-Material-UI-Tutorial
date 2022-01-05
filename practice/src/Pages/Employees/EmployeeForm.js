7a182f311df9291f21e300121b0e99e46923bbb1
Muhammad Anis <anisiqbal484@gmail.com>
2022-01-05 15:08:05 +0500

    worked on UseForm. 1 component call two function

--- practice/src/Pages/Employees/EmployeeForm.js
+++ practice/src/Pages/Employees/EmployeeForm.js
@@ -1,18 +1,12 @@
-import { Grid, makeStyles , } from "@material-ui/core";
+import { FormControl, Grid } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
 import { TextField } from "@material-ui/core";
+import {UseForm, Form} from "../../Components/UseForm";
 
 
 
 
-const useStyles = makeStyles(theme =>({
-    root:{
-        '& .MuiFormControl-root': {
-        width: '80%',
-        margin: theme.spacing(1)
-        }
-    }
-}))
+
 
 const initialFValues = {
     id:0,
@@ -28,39 +22,40 @@ const initialFValues = {
 
 function EmployeeForm() {
 
-    const [values, setValues] = useState(initialFValues);
-    const classes = useStyles();
-
-    const handleInputChange = e =>{
-        const {name, value} = e.target
-        setValues({
-            ...values,
-            [name]:value
-        })
-    }
+    const {
+        values,
+        setValues,
+        handleInputChange, 
+    }  = UseForm( initialFValues);
 
     return (
-        <form className={classes.root}>
-            <Grid container>
-                <Grid item xs={6}>
-                    <TextField
-                        variant="outlined"
-                        label="full Name"
-                        name="fullName"
-                        value={values.fullName}
-                        onChange={handleInputChange}
-                    />
-                     <TextField
-                        variant="outlined"
-                        label="Email"
-                        name="email"
-                        value={values.email}
-                        onChange={handleInputChange}
-                    />
+        
+            <Form>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <TextField
+                            variant="outlined"
+                            label="full Name"
+                            name="fullName"
+                            value={values.fullName}
+                            onChange={handleInputChange}
+                        />
+                        <TextField
+                            variant="outlined"
+                            label="Email"
+                            name="email"
+                            value={values.email}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <FormControl>
+                            
+                        </FormControl>
+                    </Grid>
                 </Grid>
-                <Grid item xs={6}></Grid>
-            </Grid>
-        </form>
+                </Form>
+       
     )
 }
 
